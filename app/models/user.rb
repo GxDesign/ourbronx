fa7fa094a2d18936b5ad7fb31cc79ad481180006
Giancarlo Martinez <g.r.rmartinez49@gmail.com>
2015-10-16 17:57:49 -0400

    add paperclip and mailer settings

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, styles: { medium: "300x300>", small: "150x150>", thumb: "60x60#{}" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
